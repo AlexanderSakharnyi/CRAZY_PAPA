82fa21169013d3b4a70815f657ba2465f2b4f85f
AlexanderSakharnyi <118662070+AlexanderSakharnyi@users.noreply.github.com>
2022-12-03 17:24:22 +0500

    Add files via upload

---
+++ urok16_2.py
@@ -0,0 +1,15 @@
+from inp16 import *
+
+vyb = 1
+ppr = 1
+
+
+if vyb == 1:
+	print ("введите ширину прямоугольника: ")
+	spr = float(input())
+	print ("введите высоту прямоугольника: ")
+	vpr = float(input())
+	rectangle1 ()
+	print ("S прямоугольника: " ,ppr)
+
+print ("ZEU!")
